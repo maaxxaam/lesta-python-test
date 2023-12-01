96facbd890251dede688ed22c35528de5fbde49e
Maaxxaam <maaxxaam@protonmail.com>
2023-12-01 23:03:05 +0300

    Add second ring fifo implementation

--- ringfifo.py
+++ ringfifo.py
@@ -55,16 +55,61 @@ class RingQueue1(RingQueue):
             raise IndexError("Ring queue is empty")
         return self.items[self.head]
   
+class RingQueue2(RingQueue):
+    def __init__(self, capacity: int = 8, growth_factor: float = 2.0):
+        self.capacity = capacity
+        self.growth_factor = growth_factor
+        self.items = [None for i in range(capacity)]
+        self.head = 0
+        self.tail = capacity - 1
+
+    def push(self, item):
+        if item is None:
+            raise ValueError("Cannot use None as a value in ring queue")
+        nextTail = (self.tail + 1) % self.capacity
+        if self.head == nextTail and self.items[self.head] is not None:
+            new_capacity = int(self.capacity * self.growth_factor)
+            new_array = [None for i in range(new_capacity)]
+            for i in range(self.head, self.head + self.capacity):
+                new_array[i - self.head] = self.items[i - self.capacity]
+            self.items = new_array
+            self.tail = self.capacity - 1
+            nextTail = self.capacity
+            self.head = 0
+            self.capacity = new_capacity
+        self.items[nextTail] = item
+        self.tail = nextTail
+  
+    def isEmpty(self) -> bool:
+        return self.head == (self.tail + 1) % self.capacity and self.items[self.head] is None
+ 
+    def pop(self):
+        if self.isEmpty():
+            raise IndexError("Ring queue is empty")
+        value = self.items[self.head]
+        self.items[self.head] = None
+        self.head = (self.head + 1) % self.capacity
+        return value
+  
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError("Ring queue is empty")
+        return self.items[self.head]
+
 if __name__ == "__main__":
-    queue = RingQueue1(4)
-    queue.push(30)
-    queue.push(100)
-    queue.push(40)
-    print(queue.peek(), queue.pop(), queue.peek())
-    queue.push(140)
-    queue.push(70)
-    queue.push(-70)
-    queue.push(0)
-    print(queue.items)
-    print(queue.pop())
+    test_queues = [RingQueue1(4), RingQueue2(4, 1.5)]
+    for queue in test_queues:
+        queue.push(30)
+        queue.push(100)
+        queue.push(40)
+        assert queue.peek() == 30
+        assert queue.pop() == 30
+        assert queue.peek() == 100
+        queue.push(140)
+        queue.push(70)
+        queue.push(-70)
+        queue.push(0)
+        assert queue.pop() == 100
+        [queue.pop() for _ in range(5)]
+        assert queue.isEmpty()
 
