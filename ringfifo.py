9543843d907a7d3e7ff634804b64c99fed939416
Maaxxaam <maaxxaam@protonmail.com>
2023-12-04 04:20:24 +0300

    Add explanation and more comments

--- ringfifo.py
+++ ringfifo.py
@@ -20,21 +20,26 @@ class RingQueue(ABC):
 class RingQueue1(RingQueue):
     def __init__(self, capacity: int = 8):
         self.capacity = capacity
+        # None denotes an abscence of an item
         self.items = [None for i in range(capacity)]
+        # Here we add at the array's beginning, so we init head and tail at the array's end
         self.head = capacity - 1
         self.tail = capacity - 1
  
     def push(self, item):
         if item is None:
             raise ValueError("Cannot use None as a value in ring queue")
+        # If we ran out of preallocated space, expand the array and move items
         if self.head == self.tail and self.items[self.head] is not None:
             new_array = [None for i in range(self.capacity * 2)]
             start_index = 0 if self.tail + 1 == self.capacity else self.tail + 1
             for i in range(start_index, start_index + self.capacity):
                 new_array[i - start_index] = self.items[i - self.capacity]
             self.items = new_array
+            # Move head to the end of moved data
             self.head = self.capacity - 1
             self.capacity *= 2
+            # Move tail to the end of newly allocated space
             self.tail = self.capacity - 1
         self.items[self.tail] = item
         self.tail = self.capacity - 1 if self.tail == 0 else self.tail - 1
@@ -59,6 +64,7 @@ class RingQueue2(RingQueue):
     def __init__(self, capacity: int = 8, growth_factor: float = 2.0):
         self.capacity = capacity
         self.growth_factor = growth_factor
+        # None denotes an abscence of an item
         self.items = [None for i in range(capacity)]
         self.head = 0
         self.tail = capacity - 1
@@ -66,7 +72,9 @@ class RingQueue2(RingQueue):
     def push(self, item):
         if item is None:
             raise ValueError("Cannot use None as a value in ring queue")
+        # Precompute index of next item, since it will be used multiple times
         nextTail = (self.tail + 1) % self.capacity
+        # If we ran out of preallocated space, expand the array and move items
         if self.head == nextTail and self.items[self.head] is not None:
             new_capacity = int(self.capacity * self.growth_factor)
             new_array = [None for i in range(new_capacity)]
