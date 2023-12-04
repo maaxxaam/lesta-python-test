35b3683d2e8f2d8074964f2ae40f14980ab7ea6c
Maaxxaam <maaxxaam@protonmail.com>
2023-12-04 15:29:21 +0300

    Add insertion sort to improve sorting speed for small arrays

--- quickSort.py
+++ quickSort.py
@@ -28,18 +28,47 @@ def quickSort(array, low = 0, high = None):
         pivot = i
         # make recursive call to smaller region
         if pivot - low < high - pivot:
-            quickSort(array, low, pivot - 1)
+            if pivot - low < 16:
+                insertionSort(array, low, pivot - 1)
+            else:
+                quickSort(array, low, pivot - 1)
             low = pivot + 1
         else:
-            quickSort(array, pivot + 1, high)
+            if high - pivot < 16:
+                insertionSort(array, pivot + 1, high)
+            else:
+                quickSort(array, pivot + 1, high)
             high = pivot - 1
 
+def insertionSort(array, low = 0, high = None):
+    if high is None:
+        high = len(array)
+    for step in range(low + 1, high):
+        key = array[step]
+        j = step - 1
+
+        while j >= low and key < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+
+        array[j + 1] = key
+
+
 if __name__ == "__main__":
-    toSort = [list() for _ in range(10)]
-    for i in range(10):
-        toSort[i] = [randint(0, 10**9) for _ in range(16)]
-    start = perf_counter()
-    for i in range(10):
-        quickSort(toSort[i])
-    end = perf_counter()
-    print((end - start) / 10)
+    sizes = [20*i for i in range(5, 20)]
+    samples = 40
+    for size in sizes:
+        print("Testing size", size)
+        toSort = [list() for _ in range(samples)]
+        for i in range(samples):
+            toSort[i] = [randint(0, 10**9) for _ in range(size)]
+        start = perf_counter()
+        for i in range(samples):
+            quickSort(toSort[i])
+        end = perf_counter()
+        print(f"Quick: {(end - start):.6f}", end=" ")
+        start = perf_counter()
+        for i in range(samples):
+            insertionSort(toSort[i])
+        end = perf_counter()
+        print(f"Insert: {(end - start):.6f}")
--- sort.py
+++ sort.py
@@ -1,31 +1,38 @@
 from typing import List
 from random import randint
 from time import perf_counter
-from quickSort import quickSort
+from quickSort import quickSort, insertionSort
 from radixSort import radixSort
 
 def mainSort(array: List[int]):
-    if len(array) < 80:
+    if len(array) < 32:
+        insertionSort(array)
+    elif len(array) < 512:
         quickSort(array)
     else:
         radixSort(array)
 
 if __name__ == '__main__':
-    sizes = [10*i for i in range(3,14)]
-    array_count = 40
+    sizes = [10**2 * i for i in range(1, 11)]
+    array_count = 500
     algos = {
-            'Quick': quickSort,
-            'Radix': radixSort
+            'Quick ': quickSort,
+            'Radix ': radixSort,
+            'Insert': insertionSort
             }
     for size in sizes:
         print('Testing size', size)
+        results = dict()
         toSort = [list() for i in range(array_count)]
         for i in range(array_count):
-            toSort[i] = [randint(0, 10**9) for i in range(size)]
+            toSort[i] = [randint(-10**9, 10**9) for i in range(size)]
         for name, func in algos.items():
             start = perf_counter()
             for i in range(array_count):
-                func(toSort[i])
+                array = toSort[i].copy()
+                func(array)
             end = perf_counter()
-            print(f"{name}: {(end - start) / array_count:.6f}")
+            print(f"{name}: {(end - start) / array_count:.9f}")
+            results[name] = (end - start) / array_count
+        print(f"Radix vs Quick: {results['Quick '] / results['Radix ']:.3f}x")
 
